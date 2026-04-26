55ce520158c7654c4727cf7628793db987a08e61
SaraPasolini <saraalmeida3606@gmail.com>
2026-04-26 17:07:12 -0300

    Exercício 1 resolvido

--- Atividade_26_Abril/Ex1/Program.cs
+++ Atividade_26_Abril/Ex1/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Produto
+{
+    protected string nome;
+    protected double preco;
+
+    public void ExibirDados()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Preço: R$ {preco}");
+    }
+}
+
+class Livro : Produto
+{
+    protected string autor;
+
+    public void ExibirDadosLivro()
+    {
+        ExibirDados();
+        Console.WriteLine($"Autor: {autor}");
+    }
+
+    // Método para atribuir valores (facilita no Main)
+    public void SetDados(string n, double p, string a)
+    {
+        nome = n;
+        preco = p;
+        autor = a;
+    }
+}
