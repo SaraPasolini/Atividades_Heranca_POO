b184e4d0410112f4c513ca5a8ed244f9977515a4
SaraPasolini <saraalmeida3606@gmail.com>
2026-04-26 18:18:15 -0300

    Atividades postadas dia 26 devabril

--- Atividade_26_Abril/Ex2/Program.cs
+++ Atividade_26_Abril/Ex2/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Pessoa
+{
+    protected string nome;
+    protected int idade;
+
+    public void ExibirDados()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Idade: {idade}");
+    }
+}
+
+class Aluno : Pessoa
+{
+    protected string curso;
+
+    public void ExibirDadosAluno()
+    {
+        ExibirDados();
+        Console.WriteLine($"Curso: {curso}");
+    }
+
+    public void SetDados(string n, int i, string c)
+    {
+        nome = n;
+        idade = i;
+        curso = c;
+    }
+}
--- Atividade_26_Abril/Ex3/Program.cs
+++ Atividade_26_Abril/Ex3/Program.cs
@@ -1,1 +1,57 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Dispositivo
+{
+    protected string marca;
+    protected string modelo;
+
+    public void ExibirDados()
+    {
+        Console.WriteLine($"Marca: {marca}");
+        Console.WriteLine($"Modelo: {modelo}");
+    }
+}
+
+class Celular : Dispositivo
+{
+    protected int armazenamento;
+
+    public void ExibirDadosCelular()
+    {
+        ExibirDados();
+        Console.WriteLine($"Armazenamento: {armazenamento}GB");
+    }
+
+    public void SetDados(string m, string mod, int arm)
+    {
+        marca = m;
+        modelo = mod;
+        armazenamento = arm;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Produto / Livro
+        Livro livro = new Livro();
+        livro.SetDados("Dom Casmurro", 39.90, "Machado de Assis");
+        Console.WriteLine("=== Livro ===");
+        livro.ExibirDadosLivro();
+
+        Console.WriteLine();
+
+        // Pessoa / Aluno
+        Aluno aluno = new Aluno();
+        aluno.SetDados("Ana", 20, "Sistemas de Informação");
+        Console.WriteLine("=== Aluno ===");
+        aluno.ExibirDadosAluno();
+
+        Console.WriteLine();
+
+        // Dispositivo / Celular
+        Celular celular = new Celular();
+        celular.SetDados("Samsung", "Galaxy S21", 128);
+        Console.WriteLine("=== Celular ===");
+        celular.ExibirDadosCelular();
+    }
+}
