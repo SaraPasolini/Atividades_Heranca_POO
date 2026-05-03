72607dfc2a4f54213e9146e97d471dba6e1621ad
SaraPasolini <saraalmeida3606@gmail.com>
2026-05-03 20:49:07 -0300

    parte 1

--- Atividade_03_Maio/Ex1/Program.cs
+++ Atividade_03_Maio/Ex1/Program.cs
@@ -1,1 +1,57 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SistemaEmpresaBiblioteca
+{
+    
+    interface IBonus
+    {
+        double CalcularBonus();
+    }
+
+    // Classe base
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public double Salario { get; set; }
+
+        public virtual void ExibirDados()
+        {
+            Console.WriteLine($"Nome: {Nome}");
+            Console.WriteLine($"Idade: {Idade}");
+            Console.WriteLine($"Salário: R$ {Salario}");
+        }
+    }
+
+    class Gerente : Funcionario, IBonus
+    {
+        public string Departamento { get; set; }
+
+        public override void ExibirDados()
+        {
+            base.ExibirDados();
+            Console.WriteLine($"Departamento: {Departamento}");
+        }
+
+        public double CalcularBonus()
+        {
+            return Salario * 0.20;
+        }
+    }
+
+    class Desenvolvedor : Funcionario, IBonus
+    {
+        public string Linguagem { get; set; }
+
+        public override void ExibirDados()
+        {
+            base.ExibirDados();
+            Console.WriteLine($"Linguagem: {Linguagem}");
+        }
+
+        public double CalcularBonus()
+        {
+            return Salario * 0.10;
+        }
+    }
+}
