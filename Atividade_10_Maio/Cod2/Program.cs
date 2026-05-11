614a24dfb89a70308b7a098c703005fd4f31a3bb
SaraPasolini <saraalmeida3606@gmail.com>
2026-05-10 22:35:33 -0300

    resposta 2

--- Atividade_10_Maio/Cod2/Program.cs
+++ Atividade_10_Maio/Cod2/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Veiculo
+{
+    public virtual void Mover()
+    {
+        Console.WriteLine("O veículo está se movendo.");
+    }
+}
+
+class Carro : Veiculo
+{
+    public override void Mover()
+    {
+        Console.WriteLine("O carro está dirigindo na estrada.");
+    }
+}
+
+class Bicicleta : Veiculo
+{
+    public override void Mover()
+    {
+        Console.WriteLine("A bicicleta está sendo pedalada.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Veiculo v1 = new Carro();
+        Veiculo v2 = new Bicicleta();
+
+        v1.Mover();
+        v2.Mover();
+    }
+}
