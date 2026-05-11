5a37337b4ebf133878e5cf698100cb4994455ab0
SaraPasolini <saraalmeida3606@gmail.com>
2026-05-10 22:36:52 -0300

    Resposta 3

--- Atividade_10_Maio/Cod3/Program.cs
+++ Atividade_10_Maio/Cod3/Program.cs
@@ -1,1 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Funcionario
+{
+    public virtual void ExibirDados()
+    {
+        Console.WriteLine("Funcionário cadastrado no sistema.");
+    }
+}
+
+class Gerente : Funcionario
+{
+    public override void ExibirDados()
+    {
+        // Chama o método da classe pai
+        base.ExibirDados();
+
+        Console.WriteLine("O gerente também é responsável por liderar a equipe.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Gerente gerente = new Gerente();
+
+        gerente.ExibirDados();
+    }
+}
